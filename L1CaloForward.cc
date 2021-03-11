5d69806899b9288a35144832beac79d3208219b3
MIAN <ap14092@students3.ph.qmul.ac.uk>
2021-03-11 15:18:29 +0000

    running on new samples, Had variable still produces a single peak so .cc file code is errenous

--- L1CaloForward.cc
+++ L1CaloForward.cc
@@ -11,6 +11,7 @@
 #include <fstream>
 #include <iostream>
 #include <TGraphAsymmErrors.h>
+#include <valarray>
 
 using namespace std;
 
@@ -100,19 +101,68 @@ void L1CaloForward::Loop()
   TH1F *hTrmEfexEleEta  = new TH1F("hTrmEfexEleEta", "hTrmEfexEleEta", 20,-5.,5.);
   TH1F *hTrmEfexElePhi  = new TH1F("hTrmEfexElePhi", "hTrmEfexElePhi", 20,-5.,5.);
 
-  // Hadronic Jets 
-  TH1F *jEleEt   = new TH1F("hL1EleEtHad",  "hL1EleEtHad",  20,10.,100.);
-  TH1F *jEleEta  = new TH1F("hL1EleEtaHad", "hL1EleEtaHad", 20,-5.,5.);
-  TH1F *jElePhi  = new TH1F("hL1ElePhiHad", "hL1ElePhiHad", 20,-5.,5.);
+
+
+  // Hadronic Jet
+//   TH1F *jEleEt   = new TH1F("hL1EleEtHad",  "hL1EleEtHad",  20,10.,100.);
+//   TH1F *jEleEta  = new TH1F("hL1EleEtaHad", "hL1EleEtaHad", 20,-5.,5.);
+//   TH1F *jElePhi  = new TH1F("hL1ElePhiHad", "hL1ElePhiHad", 20,-5.,5.);
 
   //
-  TH1F *hL1EleEt   = new TH1F("hjEleEt",  "hjEleEt",  20,10.,100.);
-  TH1F *hjEleEta  = new TH1F("hjEleEta", "hjEleEta", 20,-5.,5.);
-  TH1F *hjElePhi  = new TH1F("hjElePhi", "hjElePhi", 20,-5.,5.);
+//   TH1F *hL1EleEt   = new TH1F("hjEleEt",  "hjEleEt",  20,10.,100.);
+//   TH1F *hjEleEta  = new TH1F("hjEleEta", "hjEleEta", 20,-5.,5.);
+//   TH1F *hjElePhi  = new TH1F("hjElePhi", "hjElePhi", 20,-5.,5.);
   //Jet rejection variables
-  TH1F *hL11EleIso   = new TH1F("hL1EleIso",  "hL1EleIso",  20,-50000.,100000.);
-  TH1F *hL1EleHad   = new TH1F("hL1EleHad",  "hL1EleHad",  20,-5000.,30000.);
+  TH1F *hL1EleIso   = new TH1F("hL1EleIso",  "hL1EleIso",  20,-10000.,40000.);
+  TH1F *hL1EleHad   = new TH1F("hL1EleHad",  "hL1EleHad",  20,-1.,1.);
+
+  TH1F *hL1EleRange   = new TH1F("hL1EleRange",  "hL1EleRange",  20,0.,1.);
+  TH1F *hL1HadRange   = new TH1F("hL1HadRange",  "hL1HadRange",  20,-2.,2.);
+
+  //cuts histograms - unusable
+  // TH1F *hL1EleEtaCut   = new TH1F("hL1EleEtaCut",  "hL1EleEtaCut",  20,-2.,2.);
+  // TH1F *hL1ElePtCut   = new TH1F("hL1ElePtCut",  "hL1ElePtCut",  20,-2.,2.);
+
+  //Creating histograms for varying pt and eta cuts
+  /* TString cutNames[9];
+   cutNames[0] = "EtaCut1", cutNames[1] = "EtaCut2", cutNames[2] = "EtaCut3", cutNames[3] = "EtaCut4", cutNames[4] = "EtaCut5";
+     cutNames[5] = "PtCut1", cutNames[6] = "PtCut2", cutNames[7] = "PtCut3", cutNames[8] = "PtCut4", cutNames[9] = "PtCut5"; */
   
+  //int effiso[5] ={0}; 
+  std::valarray<int> effiso(0,5);
+
+  TH1F *hL1EtaCut1   = new TH1F("hL1EtaCut1",  "hL1EtaCut1",  10, -6., 6.);
+  TH1F *hL1EtaCut2   = new TH1F("hL1EtaCut2",  "hL1EtaCut2",  10, -6., 6.);
+  TH1F *hL1EtaCut3   = new TH1F("hL1EtaCut3",  "hL1EtaCut3",  10, -6., 6.);
+  TH1F *hL1EtaCut4   = new TH1F("hL1EtaCut4",  "hL1EtaCut4",  10, -6., 6.);
+  TH1F *hL1EtaCut5   = new TH1F("hL1EtaCut5",  "hL1EtaCut5",  10, -6., 6.);
+
+  TH1F *hL1PtCut1   = new TH1F("hL1PtCut1",  "hL1PtCut1",  20, 0., 110.);
+  TH1F *hL1PtCut2   = new TH1F("hL1PtCut2",  "hL1PtCut2",  20, 0., 110.);
+  TH1F *hL1PtCut3   = new TH1F("hL1PtCut3",  "hL1PtCut3",  20, 0., 110.);
+  TH1F *hL1PtCut4   = new TH1F("hL1PtCut4",  "hL1PtCut4",  20, 0., 110.);
+  TH1F *hL1PtCut5   = new TH1F("hL1PtCut5",  "hL1PtCut5",  20, 0., 110.);
+  TH1F* h[10] = {hL1EtaCut1, hL1EtaCut2, hL1EtaCut3, hL1EtaCut4, hL1EtaCut5,
+		hL1PtCut1, hL1PtCut2, hL1PtCut3, hL1PtCut4, hL1PtCut5};
+   
+  /*
+   int m=10;
+   int j = 1;
+   TH1F** h=new TH1F*[m];
+
+   for (int i=0; i<m;i++){
+      if(i<5){
+       cutNames[i].Append("hL1EtaCut", j); 
+       cout << cutNames[i];
+     }
+     else{
+       j = 1;
+       cutNames[i].Append("hL1PtCut", j);
+       }
+     h[i] = new TH1F("a", "a", 20., -5., 5.);
+     j++;
+     }*/
+
   int pJets = 0;
 
   //nentries = no of events
@@ -125,7 +175,7 @@ void L1CaloForward::Loop()
       Long64_t ientry = LoadTree(jentry);
       if (ientry < 0) break;
       nb = fChain->GetEntry(jentry);   nbytes += nb;
-      if(jentry%100 == 0){
+      if(jentry%10000 == 0){
 	cout<<"Processing "<<jentry<<endl;
 
 	// additional debug printout
@@ -217,29 +267,97 @@ void L1CaloForward::Loop()
 	}
       }// end loop over truth electrons
       
+      float isorange = 0;
+      float hadrange = 0;
+
+      // float isocuts[10] = { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 };
+      float isocuts[5] = { 0.3, 0.4, 0.5, 0.6,  0.7};
+      float cutrejrate[5] ={0};
+
       //rejection vars
        for (int j = 0;j<nl1iel;j++){
-	      if (Run3_jEle_Et->at(j)<10000.) continue;
+	 if (Run3_jEle_Et->at(j)>20000. && fabs(Run3_jEle_eta->at(j)) > 2.5 && fabs(Run3_jEle_eta->at(j)) < 4.5) {
                hL1EleIso->Fill(Run3_jEle_iso->at(j));
-	   }
+	       hL1EleHad->Fill(Run3_jEle_Had_New.at(j));
+	       isorange = Run3_jEle_iso->at(j)/Run3_jEle_Et->at(j);
+	       // cout<<isorange<<endl;
+	       // range of iso var of ele iso / ele et
+	       hL1EleRange->Fill(isorange);
+	       hadrange = Run3_jEle_Had_New.at(j)/Run3_jEle_Had_New.at(j);
+	       hL1HadRange->Fill(hadrange);
+	   
+	       if (0 < isorange && isorange < isocuts[0] ){
+		  effiso[0] += 1;
+	  	  h[4]->Fill(Run3_jEle_eta->at(j));
+		  h[9]->Fill(Run3_jEle_Et->at(j)/1000.);
+	       }
+	       if (0 < isorange && isorange < isocuts[1]){
+		  effiso[1] += 1;
+		  h[3]->Fill(Run3_jEle_eta->at(j));
+		  h[8]->Fill(Run3_jEle_Et->at(j)/1000.);
 
-       for (int j = 0;j<nl1hel;j++){
-            if (Run3_jEle_Had_New.at(j)<10000.) continue;
-              hL1EleHad->Fill(Run3_jEle_Had_New.at(j));
-      }   
-      
+	       }
+	       if (0 < isorange && isorange < isocuts[2]){
+		  effiso[2]++;
+		  h[2]->Fill(Run3_jEle_eta->at(j));
+		  h[7]->Fill(Run3_jEle_Et->at(j)/1000.);
+	       }
+	       if (0 < isorange && isorange < isocuts[3]){
+		  effiso[3]++;
+		  h[1]->Fill(Run3_jEle_eta->at(j));
+		  h[6]->Fill(Run3_jEle_Et->at(j)/1000.);
+	       }
+	       if (0 < isorange && isorange < isocuts[4]){
+		  effiso[4]++;
+		  h[0]->Fill(Run3_jEle_eta->at(j));
+		  h[5]->Fill(Run3_jEle_Et->at(j)/1000.);
+
+	       }
+	       /*   
+		else if (isorange > isocuts[4] && isorange < isocuts[5]){
+		  effiso6++;
+	       }
+	        else if (isorange > isocuts[5] && isorange < isocuts[6]){
+		  effiso7++;
+	       }
+	        else if (isorange > isocuts[6] && isorange < isocuts[7]){
+		  effiso8++;
+	       }
+	        else if (isorange > isocuts[7] && isorange < isocuts[8]){
+		  effiso9++;
+	       }
+	        else if (isorange > isocuts[9] && isorange < isocuts[10]){
+		  effiso10++;
+		  } */
+	 }
+       }
        for (int j = 0;j<nl1el;j++){
-            if (Run3_jEle_Et->at(j)<10000.) continue;
+	 if (Run3_jEle_Et->at(j)<10000.){
 	    pJets++;
 	      // Matched had jets electrons
-              hL1EleEtHad->Fill(Run3_jEle_Et->at(j)/1000.);
-              hL1EleEtaHad->Fill(Run3_jEle_eta->at(j));
-	      hL1ElePhiHad->Fill(Run3_jEle_phi->at(j));
+             //  hL1EleEtHad->Fill(Run3_jEle_Et->at(j)/1000.);
+             //  hL1EleEtaHad->Fill(Run3_jEle_eta->at(j));
+	      // hL1ElePhiHad->Fill(Run3_jEle_phi->at(j));
+	 }
       }   
       
    }// end event loop
+
+   cout << "cuts rejections (increments of .2"<<endl;
+   cout << effiso[0] << endl;
+   cout << "why wtf where" << endl;
+   for(int i=0; i<effiso.size(); i++){
+     int j = effiso[i];
+     cout << j/nentries << endl;
+     cout << j << endl;
+   }
+   cout << "pls" << endl;
+
    float rJets = float(pJets)/float(nentries);
    //output 
+
+
+
    cout<<nentries<<endl;
    cout<<pJets<<endl;
    cout<<rJets<<endl;
@@ -260,13 +378,20 @@ void L1CaloForward::Loop()
    hTrmEfexEleEt->Write();
    hTrmEfexEleEta->Write();
    hTrmEfexElePhi->Write();
-   hL1EleEtHad->Write();
-   hL1EleEtaHad->Write();
-   hL1ElePhiHad->Write();
+  //  hL1EleEtHad->Write();
+   // hL1EleEtaHad->Write();
+  //  hL1ElePhiHad->Write();
    hL1EleIso->Write();
    hL1EleHad->Write();
-   
-  
+   hL1EleRange->Write();
+   hL1HadRange->Write();
+
+   // hL1EtaCut->Write();
+   // hL1PtCut->Write();
+
+   for(int i=0; i<10; i++){
+     h[i]->Write();
+   }  
   
    // Efficiency plots
 
@@ -389,3 +514,4 @@ void L1CaloForward::Loop()
    can1->Print("EffEfexElphi.png","png"); 
 
 }// end Loop method
+
